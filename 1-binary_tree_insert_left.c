d202379e7ed8e5b238010f27e4112269ca69564e
TarekM007 <tarekmoustafa36@gmail.com>
2024-02-27 11:57:06 -0800

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	left_child = binary_tree_node(parent, value)
+	left_child = binary_tree_node(parent, value);
 	if (left_child == NULL)
 		return (NULL);
 
