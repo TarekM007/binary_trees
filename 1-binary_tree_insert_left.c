a308945f2c41ec42319923fb50e34428c463c5a0
TarekM007 <tarekmoustafa36@gmail.com>
2024-02-27 11:40:16 -0800

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,30 +10,16 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *left_child;
+
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
-
-	if (node == NULL)
-	{
+	left_child = binary_tree_node(parent, value);
+	if (left_child == NULL)
 		return (NULL);
-	}
-
-	binary_tree_t *left_child = parent->left;
-
-	node->n = value;
-	node->parent = parent;
-	node->left = left_child;
-	node->right = NULL;
-
-	parent->left = node;
-
-	if (left_child != NULL)
-	{
-		left_child->parent = node;
-	}
-
-	return (node);
+	left_child->left = parent->left;
+	if (left_child->left != NULL)
+		left_child->left->parent = left_child;
+	parent->left = left_child;
+	return (left_child);
 }
