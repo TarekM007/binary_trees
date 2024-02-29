441a03877e81849f1e2358045cb46876bfbd55a3
TarekM007 <tarekmoustafa36@gmail.com>
2024-02-29 06:16:02 -0800

    17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -17,9 +17,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (parent->left == node)
 		return (parent->right);
-
-	else if (parent->right == node)
-		return (parent->left);
-	else
-		return (NULL);
+	return (parent->left);
 }
